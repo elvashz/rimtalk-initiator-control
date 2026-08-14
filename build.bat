8444bcbbe48d324ace64d6573f8725388338a4ad
elvashz <137006302+elvashz@users.noreply.github.com>
2026-08-14 22:43:39 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Building RimTalk - Foreign Initiator Control...
-dotnet build -c Release
-if errorlevel 1 (echo Build failed.&pause&exit /b 1)
-echo Build succeeded.
-pause
